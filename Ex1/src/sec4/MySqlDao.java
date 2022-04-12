279a2df76813fb2d08cac7f6c7d540b999e10f76
sohe1036 <sohecc36@naver.com>
2022-04-13 04:03:20 +0900

    java413

--- Ex1/src/sec4/MySqlDao.java
+++ Ex1/src/sec4/MySqlDao.java
@@ -6,17 +6,14 @@ public class MySqlDao implements DataAccessObject {
 	public void select() {
 		System.out.println("MySql DB에서 검색");
 	}
-
 	@Override
 	public void insert() {
 		System.out.println("MySql DB에 삽입");
 	}
-
 	@Override
 	public void update() {
 		System.out.println("MySql DB에서 검색");
 	}
-
 	@Override
 	public void delete() {
 		System.out.println("MySql DB에서 삭제");
--- Ex1/src/sec4/OracleDao.java
+++ Ex1/src/sec4/OracleDao.java
@@ -6,17 +6,14 @@ public class OracleDao implements DataAccessObject {
 	public void select() {
 		System.out.println("Oracle DB에서 검색");
 	}
-
 	@Override
 	public void insert() {
 		System.out.println("Oracle DB에 삽입");
 	}
-
 	@Override
 	public void update() {
 		System.out.println("Oracle DB에서 검색");
 	}
-
 	@Override
 	public void delete() {
 		System.out.println("Oracle DB에서 삭제");
